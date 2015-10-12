adbfb39b3da7f81afc056cd177b9fef1b16609f3
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-10-12 14:16:04 +0300

    Nyyd saab tabelist otsida

--- functions.php
+++ functions.php
@@ -1,11 +1,25 @@
 <?php
     require_once("../config_global.php");
     $database = "if15_raiklep";
-    function getAllData(){
-        
+    function getAllData($keyword=""){
+        	if($keyword == ""){
+				//ei otsi
+				$search = "%%";
+			}else{
+				//otsime 
+				$search = "%".$keyword."%";
+				
+			}
+			
+			//vaikev22rtus sulgudes, et v2ltida errorit
+			// tekiks real ~31 table.php
+			
+			
+			
         $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-        $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
-        $stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
+        $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL AND (number_plate LIKE ? OR color like ?)");
+        $stmt->bind_param("ss", $search, $search);
+		$stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
         $stmt->execute();
         
 		// massiiv, kus hoiame autosid
@@ -61,6 +75,7 @@
     
     function updateCarData($car_id, $number_plate, $color){
         
+		
         $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
         
         $stmt = $mysqli->prepare("UPDATE car_plates SET number_plate=?, color=? WHERE id=?");
